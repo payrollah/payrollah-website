38112c171960907cc3d320f7c0c635fcc414304a
nkristoporus <nkristoporus@gmail.com>
2021-03-26 16:06:47 +0800

    fix: add back test and move router to app

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import AppNavigatorAuthenticated from "../AppNavigatorAuthenticated/AppNavigatorAuthenticated";
 import "./App.css";
 
 const App: React.FunctionComponent = () => {
   return (
     <div className="App">
-      <AppNavigatorAuthenticated />
+      <BrowserRouter>
+        <AppNavigatorAuthenticated />
+      </BrowserRouter>
     </div>
   );
 };
