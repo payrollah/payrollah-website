86c23438b5036be73ed9ad3c90fb5df08a6b9f32
nkristoporus <nkristoporus@gmail.com>
2021-03-29 21:15:38 +0800

    feat: replace modal with dialog

--- src/containers/Employees/CreateEmployee/CreateEmployee.tsx
+++ src/containers/Employees/CreateEmployee/CreateEmployee.tsx
@@ -1,14 +1,14 @@
 import {
   Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
   FormControl,
   FormControlLabel,
   FormLabel,
   LinearProgress,
-  makeStyles,
-  Modal,
-  Paper,
   Radio,
-  Typography,
 } from "@material-ui/core";
 import { Field, Form, Formik, FormikErrors, FormikValues } from "formik";
 import { TextField, RadioGroup } from "formik-material-ui";
@@ -27,89 +27,64 @@ interface Props {
   onClose: () => void;
 }
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: "absolute",
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-  buttonRight: {
-    marginLeft: "0.75rem",
-  },
-  buttonLeft: {
-    marginRight: "0.75rem",
-  },
-}));
-
 const CreateEmployee: React.FunctionComponent<Props> = ({
   open,
   onClose,
 }: Props) => {
-  const classes = useStyles();
   return (
-    <Modal
-      open={open}
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Paper className={classes.paper}>
-        <Typography variant="h5" gutterBottom>
-          New Employee
-        </Typography>
-        <Formik
-          initialValues={{
-            email: "",
-            salaryAmount: 0,
-            type: "fulltime",
-            salaryPeriod: "monthly",
-            role: "",
-            startDate: moment.now(),
-          }}
-          validate={(values) => {
-            const errors: FormikErrors<FormikValues> = {};
-            if (!values.email) {
-              errors.email = "Required";
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-            ) {
-              errors.email = "Invalid email address";
-            }
+    <Dialog open={open} onClose={onClose} fullWidth>
+      <DialogTitle>New Employee</DialogTitle>
 
-            if (!values.salaryAmount) {
-              errors.salaryAmount = "Required";
-            }
+      <Formik
+        initialValues={{
+          email: "",
+          salaryAmount: 0,
+          type: "fulltime",
+          salaryPeriod: "monthly",
+          role: "",
+          startDate: moment.now(),
+        }}
+        validate={(values) => {
+          const errors: FormikErrors<FormikValues> = {};
+          if (!values.email) {
+            errors.email = "Required";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+          ) {
+            errors.email = "Invalid email address";
+          }
 
-            if (!values.type) {
-              errors.type = "Required";
-            }
+          if (!values.salaryAmount) {
+            errors.salaryAmount = "Required";
+          }
 
-            if (!values.salaryPeriod) {
-              errors.salaryPeriod = "Required";
-            }
+          if (!values.type) {
+            errors.type = "Required";
+          }
 
-            if (!values.role) {
-              errors.role = "Required";
-            }
+          if (!values.salaryPeriod) {
+            errors.salaryPeriod = "Required";
+          }
 
-            if (!values.startDate) {
-              errors.startDate = "Required";
-            }
-            return errors;
-          }}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              setSubmitting(false);
-              alert(JSON.stringify(values, null, 2));
-            }, 500);
-          }}
-        >
-          {({ submitForm, isSubmitting }) => (
-            <Form>
+          if (!values.role) {
+            errors.role = "Required";
+          }
+
+          if (!values.startDate) {
+            errors.startDate = "Required";
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            setSubmitting(false);
+            alert(JSON.stringify(values, null, 2));
+          }, 500);
+        }}
+      >
+        {({ submitForm, isSubmitting }) => (
+          <Form>
+            <DialogContent>
               <Field
                 component={TextField}
                 name="email"
@@ -180,30 +155,23 @@ const CreateEmployee: React.FunctionComponent<Props> = ({
                 required
               />
               {isSubmitting && <LinearProgress />}
-              <div style={{ display: "flex", justifyContent: "flex-end" }}>
-                <Button
-                  variant="contained"
-                  disabled={isSubmitting}
-                  onClick={onClose}
-                  className={classes.buttonLeft}
-                >
-                  Cancel
-                </Button>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  disabled={isSubmitting}
-                  onClick={submitForm}
-                  className={classes.buttonRight}
-                >
-                  Submit
-                </Button>
-              </div>
-            </Form>
-          )}
-        </Formik>
-      </Paper>
-    </Modal>
+            </DialogContent>
+            <DialogActions>
+              <Button disabled={isSubmitting} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button
+                color="primary"
+                disabled={isSubmitting}
+                onClick={submitForm}
+              >
+                Submit
+              </Button>
+            </DialogActions>
+          </Form>
+        )}
+      </Formik>
+    </Dialog>
   );
 };
 
