ab5d248e1fbdc7608798b9a016bdf8488a2ae9a7
nkristoporus <nkristoporus@gmail.com>
2021-03-29 20:50:20 +0800

    feat: update employee salary modal + salary constants

--- src/containers/Employees/EditEmployeeSalary/EditEmployeeSalary.tsx
+++ src/containers/Employees/EditEmployeeSalary/EditEmployeeSalary.tsx
@@ -12,19 +12,15 @@ import {
 } from "@material-ui/core";
 import { Field, Form, Formik, FormikErrors, FormikValues } from "formik";
 import { TextField, RadioGroup } from "formik-material-ui";
-import { DatePicker } from "formik-material-ui-pickers";
-import moment from "moment";
 import React from "react";
-import {
-  BIWEEKLY,
-  FULL_TIME,
-  MONTHLY,
-  PART_TIME,
-} from "../../../constants/salary";
+import { BIWEEKLY, MONTHLY } from "../../../constants/salary";
 
 interface Props {
   open: boolean;
   onClose: () => void;
+  currSalaryAmount: number;
+  currSalaryPeriod: string;
+  employeeAddress: string;
 }
 
 const useStyles = makeStyles((theme) => ({
@@ -43,9 +39,11 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const CreateEmployee: React.FunctionComponent<Props> = ({
+const EditEmployeeSalary: React.FunctionComponent<Props> = ({
   open,
   onClose,
+  currSalaryAmount,
+  currSalaryPeriod,
 }: Props) => {
   const classes = useStyles();
   return (
@@ -59,45 +57,22 @@ const CreateEmployee: React.FunctionComponent<Props> = ({
     >
       <Paper className={classes.paper}>
         <Typography variant="h5" gutterBottom>
-          New Employee
+          Update Employee Salary
         </Typography>
         <Formik
           initialValues={{
-            email: "",
-            salaryAmount: 0,
-            type: "fulltime",
-            salaryPeriod: "monthly",
-            role: "",
-            startDate: moment.now(),
+            salaryAmount: currSalaryAmount,
+            salaryPeriod: currSalaryPeriod,
           }}
           validate={(values) => {
             const errors: FormikErrors<FormikValues> = {};
-            if (!values.email) {
-              errors.email = "Required";
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-            ) {
-              errors.email = "Invalid email address";
-            }
 
             if (!values.salaryAmount) {
               errors.salaryAmount = "Required";
             }
 
-            if (!values.type) {
-              errors.type = "Required";
-            }
-
             if (!values.salaryPeriod) {
               errors.salaryPeriod = "Required";
-            }
-
-            if (!values.role) {
-              errors.role = "Required";
-            }
-
-            if (!values.startDate) {
-              errors.startDate = "Required";
             }
             return errors;
           }}
@@ -108,25 +83,8 @@ const CreateEmployee: React.FunctionComponent<Props> = ({
             }, 500);
           }}
         >
-          {({ submitForm, isSubmitting }) => (
+          {({ submitForm, isSubmitting, dirty }) => (
             <Form>
-              <Field
-                component={TextField}
-                name="email"
-                type="email"
-                label="Email"
-                fullWidth
-                required
-              />
-              <br />
-              <Field
-                component={TextField}
-                label="Role"
-                name="role"
-                fullWidth
-                required
-              />
-              <br />
               <Field
                 component={TextField}
                 type="number"
@@ -153,32 +111,6 @@ const CreateEmployee: React.FunctionComponent<Props> = ({
                   />
                 </Field>
               </FormControl>
-              <br />
-              <FormControl component="fieldset" required>
-                <FormLabel component="legend">Employment Type</FormLabel>
-                <Field component={RadioGroup} name="type">
-                  <FormControlLabel
-                    value={FULL_TIME}
-                    control={<Radio disabled={isSubmitting} />}
-                    label="Full-time"
-                    disabled={isSubmitting}
-                  />
-                  <FormControlLabel
-                    value={PART_TIME}
-                    control={<Radio disabled={isSubmitting} />}
-                    label="Part-time"
-                    disabled={isSubmitting}
-                  />
-                </Field>
-              </FormControl>
-              <br />
-              <Field
-                component={DatePicker}
-                label="Start Date"
-                name="startDate"
-                fullWidth
-                required
-              />
               {isSubmitting && <LinearProgress />}
               <div style={{ display: "flex", justifyContent: "flex-end" }}>
                 <Button
@@ -192,7 +124,7 @@ const CreateEmployee: React.FunctionComponent<Props> = ({
                 <Button
                   variant="contained"
                   color="primary"
-                  disabled={isSubmitting}
+                  disabled={isSubmitting || !dirty}
                   onClick={submitForm}
                   className={classes.buttonRight}
                 >
@@ -207,4 +139,4 @@ const CreateEmployee: React.FunctionComponent<Props> = ({
   );
 };
 
-export default CreateEmployee;
+export default EditEmployeeSalary;
