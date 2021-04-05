429ccccc313ae62ab2a2d64589d75c79ffa47ab1
nkristoporus <nkristoporus@gmail.com>
2021-04-05 21:37:13 +0800

    feat: connect to metamask and contracts

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppNavigatorAuthenticated from "../AppNavigatorAuthenticated/AppNavigatorAuthenticated";
 import "./App.css";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import MomentUtils from "@date-io/moment";
+import EtherContext from "../../contexts/EtherContext";
+import { ethers } from "ethers";
+// import CompanyAbi from "../../contracts/Company";
+import { COMPANY_CONTRACT_ADDR } from "../../constants/contracts";
+import { Company__factory } from "@payrollah/payrollah-registry/dist/ts";
 
 const theme = createMuiTheme({
   overrides: {
@@ -24,15 +29,64 @@ const theme = createMuiTheme({
 });
 
 const App: React.FunctionComponent = () => {
+  const [
+    etherProvider,
+    setEtherProvider,
+  ] = useState<ethers.providers.Web3Provider>();
+  const [
+    etherSigner,
+    setEtherSigner,
+  ] = useState<ethers.providers.JsonRpcSigner>();
+  const [companyContract, setCompanyContract] = useState<ethers.Contract>();
+  const [jobContract, setJobContract] = useState<ethers.Contract>();
+  const [
+    jobCreatorContract,
+    setJobCreatorContract,
+  ] = useState<ethers.Contract>();
+  const [taskContract, setTaskContract] = useState<ethers.Contract>();
+  const [workerContract, setWorkerContract] = useState<ethers.Contract>();
+
+  useEffect(() => {
+    const { ethereum } = window as any;
+    ethereum.send("eth_requestAccounts");
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    setEtherProvider(provider);
+
+    const companyContract = Company__factory.connect(
+      COMPANY_CONTRACT_ADDR,
+      provider
+    );
+    setCompanyContract(companyContract);
+  }, []);
+
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <MuiPickersUtilsProvider utils={MomentUtils}>
-            <AppNavigatorAuthenticated />
-          </MuiPickersUtilsProvider>
-        </BrowserRouter>
-      </ThemeProvider>
+      <EtherContext.Provider
+        value={{
+          provider: etherProvider,
+          setProvider: setEtherProvider,
+          signer: etherSigner,
+          setSigner: setEtherSigner,
+          companyContract,
+          setCompanyContract,
+          jobContract,
+          setJobContract,
+          jobCreatorContract,
+          setJobCreatorContract,
+          taskContract,
+          setTaskContract,
+          workerContract,
+          setWorkerContract,
+        }}
+      >
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <MuiPickersUtilsProvider utils={MomentUtils}>
+              <AppNavigatorAuthenticated />
+            </MuiPickersUtilsProvider>
+          </BrowserRouter>
+        </ThemeProvider>
+      </EtherContext.Provider>
     </div>
   );
 };
--- src/containers/Menubar/Menubar.tsx
+++ src/containers/Menubar/Menubar.tsx
@@ -1,20 +1,71 @@
-import { AppBar, makeStyles, Toolbar, Typography } from "@material-ui/core";
-import React from "react";
+import {
+  AppBar,
+  Button,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import React, { useContext, useState } from "react";
+import MetaMaskOnboarding from "@metamask/onboarding";
+import EtherContext from "../../contexts/EtherContext";
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
   },
+  title: {
+    flexGrow: 1,
+  },
 }));
+
+const isMetaMaskInstalled = () => {
+  //Have to check the ethereum binding on the window object to see if it's installed
+  const { ethereum } = window as any;
+  return Boolean(ethereum && ethereum.isMetaMask);
+};
+
+const onClickInstall = () => {
+  const onboarding = new MetaMaskOnboarding();
+  onboarding.startOnboarding();
+};
 
 const Menubar: React.FunctionComponent = () => {
   const classes = useStyles();
+
+  const { provider, signer, setSigner } = useContext(EtherContext);
+  const [address, setAddress] = useState("");
+
+  const onClickConnect = () => {
+    if (!!provider) {
+      const signer = provider.getSigner();
+      setSigner(signer);
+      console.log(signer);
+      console.log("logged");
+    }
+  };
+
+  signer?.getAddress().then(setAddress);
+
   return (
     <AppBar position="fixed" className={classes.appBar}>
       <Toolbar>
-        <Typography variant="h6" noWrap>
+        <Typography variant="h6" noWrap className={classes.title}>
           Payrollah
         </Typography>
+        <div>
+          {!signer ? (
+            <Button
+              variant="contained"
+              onClick={!isMetaMaskInstalled() ? onClickInstall : onClickConnect}
+            >
+              {!isMetaMaskInstalled()
+                ? "Install Metamask"
+                : "Connect to Metamask"}
+            </Button>
+          ) : (
+            <div>{address}</div>
+          )}
+        </div>
       </Toolbar>
     </AppBar>
   );
