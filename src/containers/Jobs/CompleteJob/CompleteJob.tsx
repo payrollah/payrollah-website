bb26b9de29afe05fc29aac44904236299876fe61
nkristoporus <nkristoporus@gmail.com>
2021-04-08 20:01:07 +0800

    feat: mark job as done

--- src/containers/Jobs/CompleteJob/CompleteJob.tsx
+++ src/containers/Jobs/CompleteJob/CompleteJob.tsx
@@ -7,8 +7,10 @@ import {
   DialogTitle,
   LinearProgress,
 } from "@material-ui/core";
+import { Job__factory } from "@payrollah/payrollah-registry";
 import { Form, Formik } from "formik";
-import React from "react";
+import React, { useContext } from "react";
+import EtherContext from "../../../contexts/EtherContext";
 
 interface Props {
   open: boolean;
@@ -23,6 +25,8 @@ const CompleteJob: React.FunctionComponent<Props> = ({
   onClose,
   jobAddr,
 }: Props) => {
+  const { signer } = useContext(EtherContext);
+
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle id="form-dialog-title">Job Completed</DialogTitle>
@@ -31,13 +35,16 @@ const CompleteJob: React.FunctionComponent<Props> = ({
         initialValues={{
           jobAddr: jobAddr,
         }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            setSubmitting(false);
-            alert(JSON.stringify(values, null, 2));
-            // Call JobCreator contract function deployNewJob(string calldata _title, string calldata _description)
-            onClose();
-          }, 500);
+        onSubmit={async (values) => {
+          try {
+            if (signer) {
+              const jobContract = Job__factory.connect(jobAddr, signer);
+              await jobContract.completeJob();
+              onClose();
+            }
+          } catch (e) {
+            console.error(e);
+          }
         }}
       >
         {({ submitForm, isSubmitting }) => (
